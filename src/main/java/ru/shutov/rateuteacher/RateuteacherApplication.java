c665201c59842c5e4f2b334481d515eff4128bfb
eashutov <egorkamaxwell@yandex.ru>
2024-02-27 18:29:03 +0600

    Initialization commit

---
+++ src/main/java/ru/shutov/rateuteacher/RateuteacherApplication.java
@@ -0,0 +1,13 @@
+package ru.shutov.rateuteacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateuteacherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateuteacherApplication.class, args);
+	}
+
+}
