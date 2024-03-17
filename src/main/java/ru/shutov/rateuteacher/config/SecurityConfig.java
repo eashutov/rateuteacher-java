9f5192c0b4818cb78d77d0119b0ae5a968b12d9b
eashutov <egorkamaxwell@yandex.ru>
2024-03-17 21:02:56 +0600

    Added InMemory default users, default login page

--- src/main/java/ru/shutov/rateuteacher/config/SecurityConfig.java
+++ src/main/java/ru/shutov/rateuteacher/config/SecurityConfig.java
@@ -1,21 +1,43 @@
 package ru.shutov.rateuteacher.config;
 
-import jakarta.servlet.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import ru.shutov.rateuteacher.enums.Role;
 
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig  {
+
+    @Bean
+    @Primary
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password("user")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN")
+                .build();
+        UserDetails moderator = User.builder()
+                .username("moderator")
+                .password("moderator")
+                .roles("MODERATOR")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin, moderator);
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
