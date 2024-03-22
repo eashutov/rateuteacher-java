3e61e35694d46c238802405aa97e0371bf64f3ed
eashutov <egorkamaxwell@yandex.ru>
2024-03-22 21:07:49 +0600

    Removed Contact entity, removed comments from css, added default admin photo

--- src/main/java/ru/shutov/rateuteacher/entities/Person.java
+++ src/main/java/ru/shutov/rateuteacher/entities/Person.java
@@ -1,10 +1,7 @@
 package ru.shutov.rateuteacher.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -15,6 +12,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class Person {
     @Id
     @Column(name = "id")
