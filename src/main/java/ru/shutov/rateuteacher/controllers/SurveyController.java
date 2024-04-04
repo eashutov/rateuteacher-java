6da211a7e86948c6e614081a27af2b1aa04d6964
eashutov <egorkamaxwell@yandex.ru>
2024-04-04 15:41:59 +0600

    Implemented request mappings

--- src/main/java/ru/shutov/rateuteacher/controllers/SurveyController.java
+++ src/main/java/ru/shutov/rateuteacher/controllers/SurveyController.java
@@ -1,5 +1,7 @@
 package ru.shutov.rateuteacher.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,27 +9,41 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.shutov.rateuteacher.dto.SurveyFormDto;
 import ru.shutov.rateuteacher.entities.Survey;
+import ru.shutov.rateuteacher.request.SurveyRequest;
+import ru.shutov.rateuteacher.services.SurveyService;
+
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/survey")
 public class SurveyController {
+    private final SurveyService surveyService;
 
     @GetMapping()
-    public String survey(@ModelAttribute Survey survey, Model model) {
-        // проверить существует ли опрос с указанным кодом
-        // проверить пройден ли он этим юзером (куки)
-        // получить из сервиса опрос и отправить на представление
-        // нужно отправить его полностью, вместе с вопросами, чтобы потом их отобразить
+    public String survey(@ModelAttribute SurveyRequest surveyRequest,
+                         Model model,
+                         HttpServletRequest request) {
+        String code = surveyRequest.getCode();
+
+        if (!surveyService.validateSurvey(request, code)) {
+            return "redirect:/?wrong";
+        }
+
+        Survey survey = surveyService.getSurvey(code);
+        System.err.println(survey.getId());
+        SurveyFormDto form = surveyService.getSurveyForm(survey);
+        model.addAttribute("form", form);
+        model.addAttribute("survey", survey);
+
         return "survey/survey";
     }
 
     @PostMapping("/publish")
-    public String publish(@ModelAttribute Survey survey, Model model) {
-        // скорее всего неправильно
-        // нужно разобраться в каком виде выводится форма на страницу survey
-        // и потом исходя из этого как-то получать ответы (разобраться как)
+    public String publish(@ModelAttribute("form") SurveyFormDto answers, HttpServletResponse response) {
+        surveyService.saveAnswers(answers);
+        surveyService.setSurveyCompleted(response, answers.getCode());
         return "redirect:/survey/redirect";
     }
 
